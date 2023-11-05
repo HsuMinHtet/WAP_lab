b3e9c9d3fbd00bdf1d0780455d810bcdec9a0a01
HsuMinHtet <hsumin.hmh@gmail.com>
2023-11-05 09:34:12 -0600

    update

--- Lab5/lab5.js
+++ Lab5/lab5.js
@@ -1,47 +1,25 @@
-/* No 1 (sum over 20 value from array)*/
+/* No 1 Function'sum'(sum over 20 value from array)*/
 const arr = [34, 22, 1, 2, 13, 4, 23];
-console.log(arr);
-console.log(arr.length);
 let result = function sum(arr) {
-     let a=0;
-     for(let i=0; i< arr.length; i++){
-         if(arr[i]>20){
-             a+=arr[i];
-         }
-     }
-    return a;
+    let a= arr.filter((elem)=> elem>20).reduce((accu,elem)=>elem+accu,0);
+    return a; 
 };
 console.log(result(arr));
 
-/* No 2 create an array for the length of string which has over 5  */
-const strArr=["Java", "Spring Boot", "Spring", "Reat","Java Script"];
-
-let  resultArr= function getNewArray(arr){
-    let j=0;
-    let newArr=[];
-    for(let i=0; i<arr.length; i++){        
-        if(arr[i].length>=5){
-            newArr[j]=arr[i];
-            j++;
-        }
-    }
+/* No 2 Function 'getNewArray'to get an array for the length of string which has over 5  */
+const strArr = ["Java", "Spring Boot", "Spring", "Reat", "Java Script"];
+let resultArr = function getNewArray(strArr) {
+    let newArr = strArr.filter((elem)=> elem.length>=5 && elem.includes('a'));
     return newArr;
 };
-
 console.log(resultArr(strArr));
 
-/* No 3 */
-const concat = (...args) => {
-    const result3 = [];
-    args.forEach(arg => {
-      if (typeof arg === 'string') {
-        result3.push(...arg);
-      } else if (Array.isArray(arg)) {
-        result3.push(...arg);
-      }
-    });
-    return result3;
-  };
-  
-  const resultTest = concat('hi', [1, 2, 3], ['Hello', 'world']);
-  console.log(resultTest); // Output: ['h', 'i', 1, 2, 3, 'Hello', 'world']
+/* No 3 Arrow Function to concat the elements of the array */
+const arrowArr = ['hi', [1, 2, 3], ['Hello', 'world']];
+/* const concat = (arrowArr) => arrowArr.flat().map(elem=>elem); */
+const concat = (arrowArr) => arrowArr.map(elem=>{
+    if(typeof elem === 'string')
+        return elem.split('');
+    return elem;
+}).flat();
+console.log(concat(arrowArr));  /* Output: ['h', 'i', 1, 2, 3, 'Hello', 'world'] */
