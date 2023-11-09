f7bef806bb0a494271aa40577246b9578af25b25
HsuMinHtet <hsumin.hmh@gmail.com>
2023-11-09 09:08:00 -0600

    Update day8_no1.js

---
+++ Day8/day8_no1.js
@@ -0,0 +1,37 @@
+function LinkedList(data) {
+    this.data = data;
+    this.next = null;
+}
+
+LinkedList.prototype.add = function (data) {
+    if (this.next == null) {
+        this.next = new LinkedList(data);
+    }
+    else{
+        this.next.add(data);
+    }
+}
+
+LinkedList.prototype.print = function () {
+    console.log(this.data+"and"+ this.next);
+    if (this.next) {
+        this.next.print();
+    }
+
+}
+//prev???
+LinkedList.prototype.remove= function(val,prev){
+    if(this.data===val){
+        prev.next=this.next;
+    }else{
+        if(this.next)
+        this.next.remove(val,this);
+    }
+}
+
+linkedlist = new LinkedList(1);
+linkedlist.add(2);
+linkedlist.add(3);
+linkedlist.print(); //Expected Result: LinkedList{1,2,3};
+linkedlist.remove(2);
+linkedlist.print(); //Expected Result: LinkedList{1,3};
